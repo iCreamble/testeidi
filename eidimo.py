cf8188cf0c53e534a354ea486461c91d3fa9de0f
iCreamble <52177972+iCreamble@users.noreply.github.com>
2024-05-02 10:59:05 -0300

    review

--- eidimo.py
+++ eidimo.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from seatable_api import Base, context
 
 
@@ -32,7 +32,7 @@ with tab1:
         if st.form_submit_button('Solicitar'):
 
             dict = {}
-            dict['Data'] = '{}'.format(datetime.strftime(datetime.now(), '%d/%m/%Y %H:%M'))
+            dict['Data'] = '{}'.format(datetime.strftime(datetime.now()-timedelta(hours=3), '%d/%m/%Y %H:%M'))
             dict['Codigo'] = input1
             dict['Quantidade'] = input2
             dict['Multiplo'] = input3
